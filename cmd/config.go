ad990493052be3fff16d1acde345fd17ae6bef83
sounish nath <flock.sinasini@gmail.com>
2024-09-14 22:16:13 +0530

    server with middleware and environment full working

---
+++ cmd/config.go
@@ -0,0 +1,5 @@
+package main
+
+type ConfigOpts struct {
+	PORT int
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"log"
-	"os"
+
+	"github.com/sounishnath003/jobprocessor/internal/utils"
 )
 
-type Config struct {
-	PORT     string
-	AppName  string
-	Hostname string
-}
-
 func main() {
-	conf := &Config{
-		PORT:     getEnv("PORT", "3000"),
-		AppName:  getEnv("AppName", "special-appname-001"),
-		Hostname: getEnv("Hostname", "apple-macbook-001"),
+
+	// Defining the Env Config
+	conf := ConfigOpts{
+		PORT: utils.GetEnv("PORT", 3000).(int),
 	}
 	log.Printf("config:%+v\n", conf)
-	log.Printf("server is up and running on http://localhost:%s\n", conf.PORT)
 
-}
+	srv := NewServer(&conf)
 
-func getEnv(key, fallback string) string {
-	val := os.Getenv(key)
-	if len(val) == 0 {
-		return fallback
-	}
-	return val
+	srv.MustStart()
 }
